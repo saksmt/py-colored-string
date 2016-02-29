99d882ac1e04caef24e3dea0ce4675826e09866d
Kirill Saksin <kirill.saksin@billing.ru>
2016-02-29 21:13:10 +0300

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='coloredstring',
-    version='0.1',
+    version='1.0',
     packages=['coloredstring', 'coloredstring.util', 'coloredstring.util._protected'],
     url='https://github.com/saksmt/py-colored-string.git',
     license='MIT',
     author='smt',
     author_email='smt@saksmt.tk',
-    description='Provides support for creating colorfull strings with special markup '
+    description='Provides support for creating colorful strings with special markup'
 )
