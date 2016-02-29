c6906eae242de7df65b2476ff1cdc7bf605d26c0
Kirill Saksin <kirill.saksin@billing.ru>
2016-02-29 21:13:42 +0300

    Fixed version to semver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='coloredstring',
-    version='1.0',
+    version='1.0.0',
     packages=['coloredstring', 'coloredstring.util', 'coloredstring.util._protected'],
     url='https://github.com/saksmt/py-colored-string.git',
     license='MIT',
